dc3e3a821bb8dff1f737ae80eb03d10721bd0528
abhishekmabhi44 <146988209+abhishekmabhi44@users.noreply.github.com>
2026-09-18 13:18:49 +0530

    calculator.py

---
+++ calculator.py
@@ -0,0 +1,15 @@
+def add(a, b):
+  return a + b
+def substract( a, b):
+  return a - b
+def multiply(a ,b):
+  return a*b
+def divide(a, b):
+  return a/b
+a= 10
+b= 5
+print("addition=",add(a,b))
+print("substraction=",substract(a,b))
+print("multiplication=",multiply(a,b))
+print("division=",divide(a,b))
+                      
